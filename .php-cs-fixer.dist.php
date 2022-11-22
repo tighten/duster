4fe88ba6c96fa0ac6521688ba9eb46d66feffb4a
Anthony Clark <anthonyclark@gmail.com>
2022-11-22 13:48:38 -0800

    Use new `duster.json`

--- .php-cs-fixer.dist.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Support\PhpCsFixer;
-
-$config = require __DIR__ . '/standards/.php-cs-fixer.dist.php';
-
-return $config->setFinder(PhpCsFixer::getFinder()->exclude(['tests/Fixtures']));
