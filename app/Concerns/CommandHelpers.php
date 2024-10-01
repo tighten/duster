5741067cee2c749ceea144d7912f7b432ba8affe
msamgan <mohdsamgankhan@gmail.com>
2024-09-30 21:28:16 -0400

    Moved the functions.

--- app/Concerns/CommandHelpers.php
+++ app/Concerns/CommandHelpers.php
@@ -10,4 +10,14 @@ trait CommandHelpers
     {
         render('<div class="text-green-900 bg-green-300 px-1 font-bold">>> success: ' . $message . '</div>');
     }
+
+    public function heading(string $heading): void
+    {
+        render('<div class="font-bold bg-yellow-800 px-1">=> ' . $heading . '</div>');
+    }
+
+    public function failure(string $message): void
+    {
+        render('<div class="text-red-900 bg-red-300 px-1 font-bold">!! error: ' . $message . '</div>');
+    }
 }
--- app/Support/Tool.php
+++ app/Support/Tool.php
@@ -2,10 +2,12 @@
 
 namespace App\Support;
 
-use function Termwind\render;
+use App\Concerns\CommandHelpers;
 
 abstract class Tool
 {
+    use CommandHelpers;
+
     public function __construct(
         protected DusterConfig $dusterConfig,
     ) {}
@@ -13,19 +15,4 @@ abstract class Tool
     abstract public function lint(): int;
 
     abstract public function fix(): int;
-
-    public function heading(string $heading): void
-    {
-        render('<div class="font-bold bg-yellow-800 px-1">=> ' . $heading . '</div>');
-    }
-
-    public function success(string $message): void
-    {
-        render('<div class="text-green-900 bg-green-300 px-1 font-bold">>> success: ' . $message . '</div>');
-    }
-
-    public function failure(string $message): void
-    {
-        render('<div class="text-red-900 bg-red-300 px-1 font-bold">!! error: ' . $message . '</div>');
-    }
 }
